c96c83c585f70b2b28421448435a002c53c25c0b
markoz <markdrew53@gmail.com>
2020-03-08 02:33:40 -0600

    Refactor screen

--- src/screens/Anonymous.js
+++ src/screens/Anonymous.js
@@ -1,59 +1,148 @@
-/* eslint-disable react/prop-types */
+// Packages
 import React, { useState } from 'react';
-import { Dimensions, Text, View } from 'react-native';
-import { TabView } from 'react-native-tab-view';
+import { Tabs, Tab, ScrollableTab, Container } from 'native-base';
+import { FloatingAction } from 'react-native-floating-action';
 
 // Components
-import { DescriptionRoute } from '../components/TabView/Description';
-import { Counter } from '../components/Counter';
-import { Layout } from '../components/SignIn/Layout';
-import { Label } from '../components/Form/Label';
+import { DescriptionRoute } from '../components/TabView/Anonymous/Description';
+import { ClassificationRoute } from '../components/TabView/Anonymous/Classification';
+import { ReflectionRoute } from '../components/TabView/Anonymous/Reflection';
+import { SummaryRoute } from '../components/TabView/Anonymous/Summary';
 
-const initialLayout = { width: Dimensions.get('window').width };
+// Context
+import { AnoProvider } from '../context/AnoContext';
+
+// Icon
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Anonymous = () => {
-  const [index, setIndex] = useState(0);
-  const [state, setState] = useState({
-    patientSelect: '',
-    ageSelect: '',
-    imcSelect: '',
-    asaSelect: '',
-    locationSelect: '',
-    timeSelect: '',
-    assistantSelect: '',
-    supevisorSelect: '',
-    otherSelect: '',
-    problemSelect: ''
-  });
-  const [routes] = useState([
-    { key: 'description', title: 'Descripción' },
-    { key: 'classification', title: 'Clasificar' },
-    { key: 'reflect', title: 'Reflexión' }
-  ]);
-  const renderScene = ({ route, jumpTo }) => {
-    switch (route.key) {
-      case 'description':
-        return (
-          <DescriptionRoute jumpTo={jumpTo} state={state} setState={setState} />
-        );
-      case 'classification':
-        return <Counter jumpTo={jumpTo} />;
-      case 'reflect':
-        return (
-          <Layout>
-            <Label>jhh{state.patientSelect}</Label>
-            <Label> hhh{state.ageSelect}</Label>
-          </Layout>
-        );
-    }
+  // State Tab
+  const [tabIndex, setTab] = useState(0);
+  // State of description
+  const [patientSelect, setPatient] = useState('');
+  const [ageSelect, setAge] = useState('');
+  const [imcSelect, setImc] = useState('');
+  const [asaSelect, setAsa] = useState('');
+  const [locationSelect, setLocation] = useState('');
+  const [timeSelect, setTime] = useState('');
+  const [assistantSelect, setAssistant] = useState('');
+  const [supevisorSelect, setSupevisor] = useState('');
+  const [otherSelect, setOther] = useState('');
+  const [problemSelect, setProblem] = useState('');
+  const [descriptionProblem, setDescription] = useState('');
+  // State of classification
+
+  // State of reflection
+  const [radioGroup, setRadioGroup] = useState('');
+  const [radioGroup2, setRadioGroup2] = useState('');
+  const [contributingFactor, setContributingFactor] = useState('');
+  const [learnedLessons, setLearnedLessons] = useState('');
+
+  // Logs
+  const logDescription = {
+    patientSelect,
+    ageSelect,
+    imcSelect,
+    asaSelect,
+    locationSelect,
+    timeSelect,
+    assistantSelect,
+    supevisorSelect,
+    otherSelect,
+    problemSelect,
+    descriptionProblem
   };
+  const logReflection = {
+    radioGroup,
+    radioGroup2,
+    contributingFactor,
+    learnedLessons
+  };
+  const values = {
+    // State of description
+    patientSelect,
+    setPatient,
+    ageSelect,
+    setAge,
+    imcSelect,
+    setImc,
+    asaSelect,
+    setAsa,
+    locationSelect,
+    setLocation,
+    timeSelect,
+    setTime,
+    assistantSelect,
+    setAssistant,
+    supevisorSelect,
+    setSupevisor,
+    otherSelect,
+    setOther,
+    problemSelect,
+    setProblem,
+    descriptionProblem,
+    setDescription,
+    // State of classification
+    // State of reflection
+    radioGroup,
+    setRadioGroup,
+    radioGroup2,
+    setRadioGroup2,
+    contributingFactor,
+    setContributingFactor,
+    learnedLessons,
+    setLearnedLessons,
+    // Logs
+    logDescription,
+    logReflection
+  };
+  const handleChangeTab = () => {
+    setTab(tabIndex + 1);
+  };
+  const handleTab = tabChange => {
+    setTab(tabChange.i);
+  };
+  console.log(tabIndex);
   return (
-    <TabView
-      navigationState={{ index, routes }}
-      renderScene={renderScene}
-      onIndexChange={setIndex}
-      initialLayout={initialLayout}
-    />
+    <Container>
+      <AnoProvider value={values}>
+        <Tabs
+          renderTabBar={() => <ScrollableTab />}
+          page={tabIndex}
+          onChangeTab={tabChange => handleTab(tabChange)}
+        >
+          <Tab heading="DESCRIPCIÓN">
+            <DescriptionRoute />
+          </Tab>
+          <Tab heading="CLASIFICAR">
+            <ClassificationRoute />
+          </Tab>
+          <Tab heading="REFLEXIÓN">
+            <ReflectionRoute />
+          </Tab>
+          <Tab heading="REVISIÓN">
+            <SummaryRoute />
+          </Tab>
+        </Tabs>
+        {tabIndex === 3 ? (
+          <FloatingAction
+            animated={false}
+            showBackground={false}
+            color="#7928CA"
+            floatingIcon={<Icon name={'send'} size={30} color="#fff" />}
+            onPressMain={() => console.log('Envio de datos')}
+          />
+        ) : (
+          <FloatingAction
+            animated={false}
+            showBackground={false}
+            color="#0070F3"
+            floatingIcon={<Icon name={'check'} size={30} color="#fff" />}
+            onPressMain={() => handleChangeTab()}
+          />
+        )}
+      </AnoProvider>
+    </Container>
   );
 };
 
