9a289cb3919036a98e129790185e1ed80edbfd16
markoz <markdrew53@gmail.com>
2020-03-09 11:52:49 -0600

    Refactor styles

--- src/components/Form/FormSelect.js
+++ src/components/Form/FormSelect.js
@@ -2,11 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import RNPickerSelect from 'react-native-picker-select';
 import { withFormikControl } from 'react-native-formik';
+import { StyleSheet } from 'react-native';
 
 // Components
 import { MessageError } from './MessageError';
 
-const styles = {
+const FormSelect = ({ options, setFieldValue, error }) => {
+  return (
+    <>
+      <RNPickerSelect
+        placeholder={{
+          label: 'Seleccione...',
+          value: '',
+          color: '#9EA0A4'
+        }}
+        items={options}
+        onValueChange={setFieldValue}
+        style={{ ...styles }}
+        useNativeAndroidPickerStyle={false}
+      />
+      {error && <MessageError>{error}</MessageError>}
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
   inputIOS: {
     paddingLeft: 14,
     height: 40,
@@ -23,25 +43,9 @@ const styles = {
     borderRadius: 4,
     marginBottom: 20
   }
-};
-
-const FormSelect = ({ placeholder, options, setFieldValue, error }) => {
-  return (
-    <>
-      <RNPickerSelect
-        placeholder={placeholder}
-        items={options}
-        onValueChange={setFieldValue}
-        style={{ ...styles }}
-        useNativeAndroidPickerStyle={false}
-      />
-      {error && <MessageError>{error}</MessageError>}
-    </>
-  );
-};
+});
 
 FormSelect.propTypes = {
-  placeholder: PropTypes.object.isRequired,
   options: PropTypes.array.isRequired,
   error: PropTypes.string,
   setFieldValue: PropTypes.any
